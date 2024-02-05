a0c45df162daee26433f0def42c61566f91bb70e
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-02-05 15:58:16 +0100

    integrating Stripe within laravel 🚀

---
+++ app/Http/Controllers/StripeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
+
+class StripeController extends Controller
+{
+    public function checkout(Request $request)
+    {
+        Stripe::setApiKey(env('STRIPE_SECRET'));
+
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price_data' => [
+                    'currency' => 'usd',
+                    'product_data' => [
+                        'name' => $request->name,
+                    ],
+                    // the unit amount is in cents
+                    'unit_amount' => 2000,
+                ],
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => route('success'),
+            'cancel_url' => route('welcome')
+        ]);
+
+/*        dd($session);*/
+
+        return redirect($session->url);
+    }
+
+    public function success()
+    {
+        return view('success');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StripeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+Route::post('/checkout', [StripeController::class, 'checkout'])->name('checkout');
+Route::get('/success', [StripeController::class, 'success'])->name('success');
